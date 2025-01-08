8062b026cbb557182e8d3725e2bae662ec4ec661
Pedro Menezes <pdrmenezes1@gmail.com>
2025-01-07 23:12:57 -0300

    fix: major problem

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/basic-counter/',
+  base: "/basic-counter/",
   plugins: [react()],
   resolve: {
     alias: {
