b0f7a9a44f9cceea003fd42eda506ad3cb92d549
Pedro Menezes <pdrmenezes1@gmail.com>
2025-01-07 22:53:05 -0300

    feat: improve layout

--- src/components/counter.tsx
+++ src/components/counter.tsx
@@ -10,10 +10,9 @@ import {
   Check,
   ChevronRight,
   MinusCircle,
-  Pencil,
+  Plus,
   PlusCircle,
   Trash,
-  WrapText,
 } from "lucide-react";
 import { FormEvent, useState } from "react";
 import { Button } from "./ui/button";
@@ -61,106 +60,99 @@ export function Counter({
     >
       <Accordion type="single" collapsible className="w-full">
         <AccordionItem value={id}>
-          <div className="flex items-center justify-between gap-2 mb-4">
+          <div className="flex items-center justify-between gap-1 sm:gap-2">
             <div className="inline-flex items-center gap-2">
               <AccordionTrigger
                 className={`text-white flex items-center gap-2 group ${
                   subCounters.length === 0 && "pointer-events-none"
                 }`}
               >
-                {subCounters.length > 0 && (
-                  <ChevronRight className="group-data-[state=open]:rotate-90 group-hover:text-neutral-300 size-4 transition-transform" />
-                )}
-                {!isEditing && (
-                  <>
-                    <span className="text-lg font-semibold text-white">
-                      {name}
-                    </span>
-                    {subCounters.length > 0 && (
-                      <span className="text-neutral-400">
-                        ({subCounters.length})
-                      </span>
-                    )}
-                  </>
-                )}
+                <ChevronRight
+                  className={`${
+                    subCounters.length === 0 && "invisible pointer-events-none"
+                  } group-data-[state=open]:rotate-90 group-hover:text-lime-500 size-4 transition-transform`}
+                />
               </AccordionTrigger>
-              {isEditing && (
-                <form onSubmit={handleNameSubmit} className="flex-1">
-                  <Input
-                    type="text"
-                    value={newName}
-                    onChange={(e) => setNewName(e.target.value)}
-                    className="bg-neutral-700 text-white"
-                    autoFocus
-                  />
-                </form>
-              )}
-
-              {isEditing ? (
-                <Button
-                  size="icon"
-                  variant="unstyled"
-                  className="hover:text-lime-500"
-                  onClick={handleNameSubmit}
-                >
-                  <Check />
-                </Button>
+              {!isEditing ? (
+                <>
+                  <span
+                    onClick={() => setIsEditing(true)}
+                    className="text-left text-sm sm:text-base md:text-lg font-semibold"
+                  >
+                    {name}
+                  </span>
+                  {subCounters.length > 0 && (
+                    <span className="text-sm sm:text-base text-neutral-400">
+                      ({subCounters.length})
+                    </span>
+                  )}
+                </>
               ) : (
-                <Button
-                  size="icon"
-                  variant="unstyled"
-                  className="hover:text-lime-500"
-                  onClick={() => setIsEditing(!isEditing)}
-                >
-                  <Pencil className="p-0.5" />
-                </Button>
+                <>
+                  <form onSubmit={handleNameSubmit} className="flex-1">
+                    <Input
+                      type="text"
+                      value={newName}
+                      onChange={(e) => setNewName(e.target.value)}
+                      className="bg-neutral-700 h-8 sm:h-10 text-xs sm:text-base"
+                      autoFocus
+                    />
+                  </form>
+                  <Button
+                    size="icon"
+                    variant="icon"
+                    className="hover:text-lime-500"
+                    onClick={handleNameSubmit}
+                  >
+                    <Check />
+                  </Button>
+                </>
               )}
             </div>
 
-            <div className="space-x-4  inline-flex items-center">
-              <div className="space-x-2.5 inline-flex items-center">
+            <div className="space-x-2.5 sm:space-x-4 inline-flex items-center flex-shrink-0">
+              <div className="space-x-1 sm:space-x-2 inline-flex items-center">
                 <Button
                   size="icon"
-                  variant="unstyled"
+                  variant="icon"
                   className="hover:text-lime-500"
                   onClick={() => incrementCounter(id)}
                 >
                   <PlusCircle />
                 </Button>
-                <span className="text-lg font-semibold text-white">
+                <span className="text-sm sm:text-base md:text-lg font-semibold ">
                   {value}
                 </span>
                 <Button
                   size="icon"
-                  variant="unstyled"
+                  variant="icon"
                   className="hover:text-lime-500"
                   onClick={() => decrementCounter(id)}
                 >
                   <MinusCircle />
                 </Button>
               </div>
-              <div className="space-x-2.5">
-                <Button
-                  size="icon"
-                  variant="outline"
-                  onClick={() => addCounter(id, "New Sub-Counter")}
-                  className="bg-neutral-700 text-white hover:bg-neutral-600 hover:text-lime-500"
-                  title="Create Sub-Counter"
-                >
-                  <WrapText className="size-4" />
-                </Button>
-                <Button
-                  size="icon"
-                  variant="outline"
-                  onClick={() => deleteCounter(id)}
-                  className="bg-neutral-700 text-white hover:bg-neutral-600 hover:text-lime-500"
-                  title="Delete Counter"
-                >
-                  <Trash className="size-4" />
-                </Button>
-              </div>
+              <Button
+                size="icon"
+                variant="outline"
+                onClick={() => deleteCounter(id)}
+                className="bg-neutral-700 hover:bg-neutral-600 hover:text-lime-500"
+                title="Delete Counter"
+              >
+                <Trash className="size-4" />
+              </Button>
             </div>
           </div>
+          <Button
+            variant="unstyled"
+            size="unstyled"
+            className="text-neutral-500 hover:text-lime-500 mb-4 text-xs sm:text-sm pl-4 py-1"
+            onClick={() => addCounter(id, "New Sub-Counter")}
+            title="Create Sub-Counter"
+          >
+            <Plus className="h-4 w-4 mr-1" />
+            Add Sub-Counter
+          </Button>
 
           <AccordionContent>
             <hr className=" border-neutral-600 mb-2" />
